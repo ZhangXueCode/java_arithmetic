92e50a9d34ad7eced3ecd14d58b137f04b6e7e82
hhzxa <zxcvzxcv9699@qq.com>
2025-10-31 14:26:25 +0800

    1

--- bitwiseOperate/src/Test.java
+++ bitwiseOperate/src/Test.java
@@ -12,6 +12,9 @@ public class Test {
     }
     //判断字符是否唯一
     public boolean isUnique(String astr) {
+        if(astr.length() > 26) {
+            return false;
+        }
         int bitMap = 0;
         for (int i = 0; i < astr.length(); i++) {
             int x = astr.charAt(i) - 'a';
@@ -21,5 +24,15 @@ public class Test {
             bitMap |= 1 << x;
         }
         return true;
+    }
+    //两数相加
+    public int sum(int a,int b) {
+        while (b != 0) {
+            int x = a ^ b;
+            int c = (a & b) << 1;
+            a = x;
+            b = c;
+        }
+        return a;
     }
 }
