917ff72b2140e32f927d7d02dc425ceb9062c694
hhzxa <zxcvzxcv9699@qq.com>
2025-11-04 18:27:44 +0800

    1

--- dp/Dp/src/Test.java
+++ dp/Dp/src/Test.java
@@ -474,6 +474,31 @@ public class Test {
         return sum;
 
     }
+    //回文子串
+    public int countSubstrings(String ss) {
+        int n = ss.length();
+        char[] s = ss.toCharArray();
+        boolean[][] dp = new boolean[n][n];
+        int sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= i ; j--) {
+                if(s[i] != s[j]) {
+                    dp[i][j] = false;
+                }else {
+                    if(i == j || i + 1 == j) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if(dp[i][j] == true) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+
+    }
 
 
 
