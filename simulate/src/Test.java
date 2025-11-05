0634b2f3198334cb29b55a283ce292c0edcb41b9
hhzxa <zxcvzxcv9699@qq.com>
2025-11-05 18:55:34 +0800

    1

--- simulate/src/Test.java
+++ simulate/src/Test.java
@@ -29,5 +29,33 @@ public class Test {
         return ret;
 
     }
+    //N字形变换
+    public String convert(String s, int numRows) {
+        if(numRows == 1) {
+            return s;
+        }
+        int n = s.length(),d = 2 * numRows - 2;
+        StringBuilder ret = new StringBuilder();
+        for (int i = 0; i < n; i+=d) {
+            ret.append(s.charAt(i));
+        }
+        for (int k = 1;k < numRows - 1;k++) {
+            for (int i = k,j = d - k;i < n || j < n;i+=d,j+=d) {
+                if(i < n) {
+                    ret.append(s.charAt(i));
+                }
+                if(j < n) {
+                    ret.append(s.charAt(j));
+                }
+            }
+        }
+        for (int i = numRows - 1; i < n; i+=d) {
+            ret.append(s.charAt(i));
+        }
+        return ret.toString();
+        
+
+    }
+
 
 }
