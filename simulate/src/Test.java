8baff0e818c81fea4f3cfaa6306e171b4cff7bf5
hhzxa <zxcvzxcv9699@qq.com>
2025-11-07 19:09:31 +0800

    1

--- simulate/src/Test.java
+++ simulate/src/Test.java
@@ -53,8 +53,24 @@ public class Test {
             ret.append(s.charAt(i));
         }
         return ret.toString();
-        
-
+    }
+    //外观数列
+    public String countAndSay(int n) {
+        String ret = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder tmp = new StringBuilder();
+            int len = ret.length();
+            for (int left = 0,right = 0;right < len;) {
+                while (right < len && ret.charAt(left) == ret.charAt(right)) {
+                    right++;
+                }
+                tmp.append(Integer.toString(right - left));
+                tmp.append(ret.charAt(left));
+                left = right;
+            }
+            ret = tmp.toString();
+        }
+        return ret;
     }
 
 
