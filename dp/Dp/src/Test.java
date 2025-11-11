36ee4cbb3284929470f83321c28c08bdc9d97045
hhzxa <zxcvzxcv9699@qq.com>
2025-11-11 18:51:33 +0800

    1

--- dp/Dp/src/Test.java
+++ dp/Dp/src/Test.java
@@ -602,6 +602,22 @@ public class Test {
         return dp[0][n - 1];
 
     }
+    //让字符串成为回文串的最小插入次数
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                }else {
+                    dp[i][j] = Math.min(dp[i][j - 1],dp[i + 1][j]) + 1;
+                }
+            }
+        }
+        return dp[0][n - 1];
+
+    }
 
 
 
