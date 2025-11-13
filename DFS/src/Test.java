3e0f0b43c3c7a4426a4293ed2b631e826bff2fb3
hhzxa <zxcvzxcv9699@qq.com>
2025-11-13 18:56:10 +0800

    1

--- DFS/src/Test.java
+++ DFS/src/Test.java
@@ -1,4 +1,11 @@
 import java.util.List;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
 
 public class Test {
     //汉诺塔
@@ -17,5 +24,22 @@ public class Test {
         dfs(B,A,C,n - 1);
 
     }
+    //合并两个有序链表
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+        if(list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
+        }else {
+            list2.next = mergeTwoLists(list2.next,list1);
+            return list2;
+        }
+    }
+
+
 
 }
