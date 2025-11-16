b529ce1485f326017ef4df2594184df8195433fd
hhzxa <zxcvzxcv9699@qq.com>
2025-11-16 19:07:58 +0800

    1

--- LinkedList/src/Test.java
+++ LinkedList/src/Test.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 //  Definition for singly-linked list.
 class ListNode {
@@ -87,6 +88,26 @@ public class Test {
                 cur2 = cur2.next;
             }
         }
+    }
+    //合并k个有序链表
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((v1,v2) -> v1.val - v2.val);
+        for(ListNode head : lists) {
+            if(head != null) {
+                heap.offer(head);
+            }
+        }
+        ListNode ret = new ListNode();
+        ListNode pre = ret;
+        while (!heap.isEmpty()) {
+            ListNode t = heap.poll();
+            pre.next = t;
+            pre = t;
+            if(t.next != null) {
+                heap.offer(t.next);
+            }
+        }
+        return ret.next;
     }
 
 
