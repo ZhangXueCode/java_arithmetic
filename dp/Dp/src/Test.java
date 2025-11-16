d3177aecc2372e5c7486b11181327dfdfcd70dfd
hhzxa <zxcvzxcv9699@qq.com>
2025-11-16 19:08:17 +0800

    1

--- dp/Dp/src/Test.java
+++ dp/Dp/src/Test.java
@@ -652,6 +652,28 @@ public class Test {
         }
         return dp[m][n];
     }
+    //不同的子序列
+    public int numDistinct(String s, String t) {
+        s = " " + s;
+        t = " " + t;
+        int m = t.length();
+        int n = s.length();
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if(s.charAt(j) == t.charAt(i)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                dp[i][j] += dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+
+    }
+
 
 
 
