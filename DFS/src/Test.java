fe564615adacd0de79730e0bd48c3e4e07ef5f0e
hhzxa <zxcvzxcv9699@qq.com>
2025-11-16 19:07:41 +0800

    1

--- DFS/src/Test.java
+++ DFS/src/Test.java
@@ -39,6 +39,17 @@ public class Test {
             return list2;
         }
     }
+    //反转链表
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
 
 
 
