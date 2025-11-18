1e68e9a92517cc0f5bc54badd0cd39ad3040f4fa
hhzxa <zxcvzxcv9699@qq.com>
2025-11-18 20:18:41 +0800

    1

--- DFS/src/Test.java
+++ DFS/src/Test.java
@@ -60,6 +60,16 @@ public class Test {
         head.next = tmp;
         return ret;
     }
+    //快速幂
+    public double myPow(double x, int n) {
+        return n < 0 ? 1.0 / pow(x,-n) :pow(x,n);
+    }
+    public double pow(double x,int n) {
+        if(n == 0) return 1.0;
+        double tmp = pow(x,n / 2);
+        return n % 2 == 0 ? tmp * tmp : tmp * tmp * x;
+    }
+
 
 
 
