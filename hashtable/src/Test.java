1652e76c67006f043e8d5ce8143dd541df13973c
hhzxa <zxcvzxcv9699@qq.com>
2025-11-23 16:02:34 +0800

    1

--- hashtable/src/Test.java
+++ hashtable/src/Test.java
@@ -16,4 +16,22 @@ public class Test {
         }
         return new int[]{};
     }
+    //判断是否为字符重排
+    public boolean CheckPermutation(String s1, String s2) {
+        if(s1.length() != s2.length()) {
+            return false;
+        }
+        int[] hash = new int[26];
+        for(char ch : s1.toCharArray()) {
+            hash[ch - 'a']++;
+        }
+        for(char ch : s2.toCharArray()) {
+            hash[ch - 'a']--;
+            if(hash[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
