318853a63108e0eec87275588068b41daecb5000
hhzxa <zxcvzxcv9699@qq.com>
2025-11-26 17:31:09 +0800

    1

--- hashtable/src/Test.java
+++ hashtable/src/Test.java
@@ -1,7 +1,4 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Test {
     //两数之和
@@ -47,6 +44,34 @@ public class Test {
         }
         return false;
     }
+    //存在重复元素Ⅱ
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> hash = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hash.containsKey(nums[i]) && (i - hash.get(nums[i]) <= k)) {
+                return true;
+            }
+            hash.put(nums[i], i);
+        }
+        return false;
+
+    }
+    //字母异位词分组
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> hash = new HashMap<>();
+        for(String s : strs) {
+            char[] tmp = s.toCharArray();
+            Arrays.sort(tmp);
+            String key = new String(tmp);
+            if(!hash.containsKey(key)) {
+                hash.put(key,new ArrayList<>());
+            }
+            hash.get(key).add(s);
+        }
+        return new ArrayList<>(hash.values());
+
+    }
+
 
 
 }
