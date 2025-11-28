43472e925654a57f1e56d7c84033b529b66b2df6
hhzxa <zxcvzxcv9699@qq.com>
2025-11-28 17:19:09 +0800

    1

--- string/src/Test.java
+++ string/src/Test.java
@@ -11,5 +11,31 @@ public class Test {
         }
         return strs[0];
     }
+    //最长回文字串
+    public String longestPalindrome(String s) {
+        int begin = 0,len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int left = i,right = i;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            if(right - left - 1 > len) {
+                begin = left + 1;
+                len = right - left - 1;
+            }
+            left = i;
+            right = left + 1;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            if(right - left - 1 > len) {
+                begin = left + 1;
+                len = right - left - 1;
+            }
+        }
+        return s.substring(begin,len + begin);
+    }
 
 }
