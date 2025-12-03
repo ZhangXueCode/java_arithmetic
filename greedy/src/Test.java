e62e2ca37721c8473e71d7c24176d7a81f150c7f
hhzxa <zxcvzxcv9699@qq.com>
2025-12-03 20:58:18 +0800

    1

--- greedy/src/Test.java
+++ greedy/src/Test.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 public class Test {
     //柠檬水找零
     public boolean lemonadeChange(int[] bills) {
@@ -27,5 +29,23 @@ public class Test {
         }
         return true;
 
+    }
+    //将数组和减半至少操作次数
+    public int halveArray(int[] nums) {
+        PriorityQueue<Double> heap = new PriorityQueue<>((o1, o2) -> o2.compareTo(o1));
+        double sum = 0.0;
+        int c = 0;
+        for(int x : nums) {
+            heap.offer((double)x);
+            sum += x;
+        }
+        sum /= 2.0;
+        while (sum > 0) {
+            double a = heap.poll() / 2.0;
+            sum -= a;
+            c++;
+            heap.offer(a);
+        }
+        return c;
     }
 }
