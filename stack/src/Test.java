c30e709fde5ccaf2010a067de4d8b4e594096af1
hhzxa <zxcvzxcv9699@qq.com>
2025-12-10 17:59:30 +0800

    1

--- stack/src/Test.java
+++ stack/src/Test.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Stack;
 
 public class Test {
     //删除字符中所有相邻重复元素
@@ -44,6 +45,46 @@ public class Test {
         }
         return s1.toString().equals(s2.toString());
 
+    }
+    //基本计算器Ⅱ
+    public int calculate(String ss) {
+        Stack<Integer> stack = new Stack<>();
+        char op = '+';
+        char[] s = ss.toCharArray();
+        int i = 0,n = s.length;
+        while (i < n) {
+            if(s[i] == ' ') {
+                i++;
+            } else if (s[i] >= '0' && s[i] <= '9') {
+                int tmp = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    tmp *= 10;
+                    tmp += s[i] - '0';
+                    i++;
+                }
+                if(op == '+') {
+                    stack.push(tmp);
+                } else if (op == '-') {
+                    stack.push(-tmp);
+                } else if (op == '*') {
+                    int a = stack.pop();
+                    stack.push(a * tmp);
+                } else if (op == '/') {
+                    int a = stack.pop();
+                    stack.push(a / tmp);
+                }
+            }else {
+                if(s[i] == '-' || s[i] == '+' || s[i] == '*' || s[i] == '/') {
+                    op = s[i];
+                    i++;
+                }
+            }
+        }
+        int ret = 0;
+        while (!stack.isEmpty()) {
+            ret += stack.pop();
+        }
+        return ret;
     }
 
 
