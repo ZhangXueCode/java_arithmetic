0aa699017f769be29214f0e65df6d243d578b2b8
hhzxa <zxcvzxcv9699@qq.com>
2025-12-14 15:57:08 +0800

    1

--- stack/src/Test.java
+++ stack/src/Test.java
@@ -133,6 +133,22 @@ public class Test {
 
     }
 
+    //验证栈序列
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> st = new Stack<>();
+        int i = 0,j = 0;
+        while (i < pushed.length) {
+            st.push(pushed[i]);
+            while (!st.isEmpty() && st.peek() == popped[j]) {
+                st.pop();
+                j++;
+            }
+            i++;
+        }
+        return st.isEmpty();
+
+    }
+
 
 
 
