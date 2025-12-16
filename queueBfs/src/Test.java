9e31b833d2a9738f7f550bffb918a772e4a5f4ec
hhzxa <zxcvzxcv9699@qq.com>
2025-12-16 15:41:51 +0800

    1

--- queueBfs/src/Test.java
+++ queueBfs/src/Test.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 class Node {
     public int val;
@@ -17,7 +14,19 @@ class Node {
         val = _val;
         children = _children;
     }
-};
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Test {
     //N叉树的层序遍历
     public List<List<Integer>> levelOrder(Node root) {
@@ -42,8 +51,37 @@ public class Test {
             ret.add(tmp);
         }
         return ret;
-
-
     }
+    //二叉树的锯齿形层序遍历
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        int h = 1;
+        List<List<Integer>> ret = new ArrayList<>();
+        if(root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int c = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = 0; i < c; i++) {
+                TreeNode a = queue.poll();
+                tmp.add(a.val);
+                if(a.left != null) {
+                    queue.add(a.left);
+                }
+                if(a.right != null) {
+                    queue.add(a.right);
+                }
+            }
+            if(h % 2 == 0) {
+                Collections.reverse(tmp);
+            }
+            ret.add(tmp);
+            h++;
+        }
+        return ret;
+    }
+
 
 }
