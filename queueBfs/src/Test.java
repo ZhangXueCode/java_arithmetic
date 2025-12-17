e5a7d05f2fb71673ed404682bdc3c32ca00fa43c
hhzxa <zxcvzxcv9699@qq.com>
2025-12-17 17:16:25 +0800

    1

--- queueBfs/src/Test.java
+++ queueBfs/src/Test.java
@@ -27,6 +27,31 @@ class TreeNode {
         this.right = right;
     }
 }
+
+class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    // 构造方法
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    // 静态工厂方法，简化创建
+    public static <K, V> Pair<K, V> of(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    // get方法
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
 public class Test {
     //N叉树的层序遍历
     public List<List<Integer>> levelOrder(Node root) {
@@ -81,6 +106,31 @@ public class Test {
             h++;
         }
         return ret;
+    }
+    //二叉树的最大宽度
+    public int widthOfBinaryTree(TreeNode root) {
+        int ret = 0;
+        List<Pair<TreeNode,Integer>> q = new ArrayList<>();
+        q.add(new Pair<TreeNode,Integer>(root,1));
+        while (!q.isEmpty()) {
+            Pair<TreeNode,Integer> t1 = q.get(0);
+            Pair<TreeNode,Integer> t2 = q.get(q.size() - 1);
+            ret = Math.max(ret,t2.getValue() - t1.getValue() + 1);
+            List<Pair<TreeNode,Integer>> tmp = new ArrayList<>();
+            for(Pair<TreeNode,Integer> t : q) {
+                TreeNode node = t.getKey();
+                int index = t.getValue();
+                if(node.left != null) {
+                    tmp.add(new Pair<TreeNode,Integer>(node.left,index * 2));
+                }
+                if(node.right != null) {
+                    tmp.add(new Pair<TreeNode,Integer>(node.right,index * 2 + 1));
+                }
+            }
+            q = tmp;
+        }
+        return ret;
+
     }
 
 
