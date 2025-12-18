04971d2df8806a03c1e13ce9c8911ad132b1a28a
hhzxa <zxcvzxcv9699@qq.com>
2025-12-18 18:19:31 +0800

    1

--- greedy/src/Test.java
+++ greedy/src/Test.java
@@ -285,6 +285,19 @@ public class Test {
         ret[n] = left;
         return ret;
     }
+    //分发饼干
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0,j = 0;
+        while (j < s.length && i < g.length) {
+            if(s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
 
 
 
