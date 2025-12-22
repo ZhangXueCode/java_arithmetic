3eb35247a9fd463ba50c7cf6d6d1fb045d902132
hhzxa <zxcvzxcv9699@qq.com>
2025-12-22 16:58:04 +0800

    1

--- greedy/src/Test.java
+++ greedy/src/Test.java
@@ -347,6 +347,27 @@ public class Test {
         }
         return true;
     }
+    //加油站
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n = gas.length;
+        for (int i = 0; i < n; i++) {
+            int rest = 0;
+            int step = 0;
+            for (; step < n; step++) {
+                int index = (i + step) % n;
+                rest = rest + gas[index] - cost[index];
+                if(rest < 0) {
+                    break;
+                }
+            }
+            if(rest >= 0) {
+                return i;
+            }
+            i = i + step;
+        }
+        return -1;
+
+    }
 
 
 
