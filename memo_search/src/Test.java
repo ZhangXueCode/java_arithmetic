337d5977ef769a19975975634dee89572e61d847
hhzxa <zxcvzxcv9699@qq.com>
2025-12-24 16:15:48 +0800

    1

--- memo_search/src/Test.java
+++ memo_search/src/Test.java
@@ -19,4 +19,29 @@ public class Test {
         a[i][j] = dfs(i - 1,j) + dfs(i,j - 1);
         return a[i][j];
     }
+    //最长递增子序列
+    int[] arr;
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        arr = new int[n];
+        int ret = 1;
+        for (int i = 0; i < nums.length; i++) {
+            ret = Math.max(dfs(i,nums),ret);
+        }
+        return ret;
+
+    }
+    int dfs(int pose,int[] nums) {
+        if(arr[pose] != 0) {
+            return arr[pose];
+        }
+        int ret = 1;
+        for (int i = pose + 1; i < nums.length; i++) {
+            if(nums[i] > nums[pose]) {
+                ret = Math.max(dfs(i,nums),ret) + 1;
+            }
+        }
+        arr[pose] = ret;
+        return ret;
+    }
 }
