6a60e6fbfa16c73649d569e632ea73bc58fe2e93
hhzxa <zxcvzxcv9699@qq.com>
2026-01-02 15:35:23 +0800

    1

--- bfs/src/Test.java
+++ bfs/src/Test.java
@@ -414,5 +414,72 @@ public class Test {
         return ret;
 
     }
+    //地图中的最高点
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+        int[][] ret = new int[m][n];
+        for(int[] x : ret) {
+            Arrays.fill(x,-1);
+        }
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(isWater[i][j] == 1) {
+                    ret[i][j] = 0;
+                    q.add(new int[]{i,j});
+                }
+            }
+        }
+        while (!q.isEmpty()) {
+            int[] t = q.poll();
+            int a = t[0],b = t[1];
+            for (int i = 0; i < 4; i++) {
+                int x = a + dx[i];
+                int y = b + dy[i];
+                if(x >= 0 && x < m && y >= 0 && y < n && ret[x][y] == -1) {
+                    ret[x][y] = ret[a][b] + 1;
+                    q.add(new int[]{x,y});
+                }
+            }
+        }
+        return ret;
+
+    }
+    //地图分析
+    public int maxDistance(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] ret = new int[m][n];
+        for (int[] x : ret) {
+            Arrays.fill(x,-1);
+        }
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    q.add(new int[]{i,j});
+                    ret[i][j] = 0;
+                }
+            }
+        }
+        int tmp = 0;
+        while (!q.isEmpty()) {
+            int[] t = q.poll();
+            int a = t[0],b = t[1];
+            for (int i = 0; i < 4; i++) {
+                int x = a + dx[i];
+                int y = b + dy[i];
+                if(x >= 0 && x < m && y >= 0 && y < n && ret[x][y] == -1) {
+                    ret[x][y] = ret[a][b] + 1;
+                    q.add(new int[]{x,y});
+                    tmp = Math.max(tmp,ret[x][y]);
+                }
+            }
+        }
+        return tmp == 0 ? -1 : tmp;
+
+
+    }
 
 }
