5d4cee88e866e3ebebf134d8a9274037d29f7881
hhzxa <zxcvzxcv9699@qq.com>
2026-01-18 20:14:36 +0800

    1

--- test_training_week02/src/Main.java
+++ test_training_week02/src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.*;
 
 public class Main {
+    //Day-07
     //找出字符串中连续的最长字符
 //    public static void main(String[] args) throws Exception{
 //        BufferedReader br = new BufferedReader(new
@@ -60,24 +61,90 @@ public class Main {
         }
     }
     //拼三角
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        int[] arr = new int[6];
-        for(int i = 0;i < t;i++) {
-            for(int j = 0;j < 6;j++) {
-                arr[j] = in.nextInt();
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int t = in.nextInt();
+//        int[] arr = new int[6];
+//        for(int i = 0;i < t;i++) {
+//            for(int j = 0;j < 6;j++) {
+//                arr[j] = in.nextInt();
+//            }
+//            Arrays.sort(arr);
+//            if(arr[0] + arr[1] > arr[2] && arr[3] + arr[4] > arr[5] ||
+//                    arr[0] + arr[2] > arr[3] && arr[1] + arr[4] > arr[5] ||
+//                    arr[0] + arr[3] > arr[4] && arr[1] + arr[2] > arr[5] ||
+//                    arr[0] + arr[4] > arr[5] && arr[1] + arr[2] > arr[3]){
+//                System.out.println("Yes");
+//            }else {
+//                System.out.println("No");
+//            }
+//        }
+//    }
+    //Day-08
+    //求最小公倍数
+//    static int gcd(int a,int b) {
+//        if(b == 0) {
+//            return a;
+//        }
+//        return gcd(b,a % b);
+//
+//    }
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int a = in.nextInt();
+//        int b = in.nextInt();
+//        System.out.print(a * b / gcd(a,b));
+//    }
+    //数组中最长连续子序列(1,2,3...)
+    public int MLS (int[] arr) {
+        Arrays.sort(arr);
+        int ret = 1;
+        int i = 0;
+        while(i < arr.length) {
+            int j = i + 1,count = 1;
+            while(j < arr.length) {
+                if(arr[j] - 1 == arr[j - 1]) {
+                    count++;
+                    j++;
+                }else if(arr[j] == arr[j - 1]) {
+                    j++;
+                }else {
+                    break;
+                }
             }
-            Arrays.sort(arr);
-            if(arr[0] + arr[1] > arr[2] && arr[3] + arr[4] > arr[5] ||
-                    arr[0] + arr[2] > arr[3] && arr[1] + arr[4] > arr[5] ||
-                    arr[0] + arr[3] > arr[4] && arr[1] + arr[2] > arr[5] ||
-                    arr[0] + arr[4] > arr[5] && arr[1] + arr[2] > arr[3]){
-                System.out.println("Yes");
-            }else {
-                System.out.println("No");
-            }
+            ret = Math.max(ret,count);
+            i = j;
         }
+        return ret;
     }
+    //字母收集
+//    public static void main(String[] args) {
+//        Scanner in = new Scanner(System.in);
+//        int n = in.nextInt();
+//        int m = in.nextInt();
+//        char[][] a = new char[n][m];
+//        for (int i = 0; i < n; i++) {
+//            char[] s = in.next().toCharArray();
+//            for (int j = 0; j < m; j++) {
+//                a[i][j] = s[j];
+//            }
+//        }
+//        int[][] dp = new int[n + 1][m + 1];
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= m; j++) {
+//                if (a[i - 1][j - 1] == 'l') {
+//                    dp[i][j] = 4;
+//                } else if (a[i - 1][j - 1] == 'o') {
+//                    dp[i][j] = 3;
+//                } else if (a[i - 1][j - 1] == 'v') {
+//                    dp[i][j] = 2;
+//                } else if (a[i - 1][j - 1] == 'e') {
+//                    dp[i][j] = 1;
+//                }
+//                dp[i][j] += Math.max(dp[i - 1][j],dp[i][j - 1]);
+//            }
+//        }
+//        System.out.print(dp[n][m]);
+//    }
 
 }
